e2209542ec62d67f4b6b1275b2101f4fbbd2e79b
Tiger <AtlantisSpeed@gmail.com>
2022-03-15 22:26:52 +0800

    arco-cli: initialize project

---
+++ src/views/list/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <router-view />
+</template>
+
+<script lang="ts" setup></script>
+
+<style scoped lang="less">
+  .container {
+    padding: 0 20px 20px 20px;
+  }
+
+  .operations {
+    display: flex;
+  }
+</style>
